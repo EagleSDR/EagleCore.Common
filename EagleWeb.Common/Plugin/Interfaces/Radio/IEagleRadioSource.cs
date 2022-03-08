87bfc9aead225f3b3bbdedea0e54960e7520f2e3
Roman-Port <rvporterfield@gmail.com>
2022-03-08 16:55:22 -0600

    Added more events to the source object.

--- EagleWeb.Common/Plugin/Interfaces/Radio/IEagleRadioSource.cs
+++ EagleWeb.Common/Plugin/Interfaces/Radio/IEagleRadioSource.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace EagleWeb.Common.Plugin.Interfaces.Radio
 {
+    public delegate void IEagleRadioSource_SampleRateChangedEventArgs(IEagleRadioSource source, float newSampleRate);
+    public delegate void IEagleRadioSource_SamplesDroppedEventArgs(IEagleRadioSource source, long dropped);
+
     public interface IEagleRadioSource : IEagleObject, IEagleRadioModule
     {
         /// <summary>
@@ -17,6 +20,16 @@ namespace EagleWeb.Common.Plugin.Interfaces.Radio
         /// Gets the current center frequency.
         /// </summary>
         long CenterFrequency { get; set; }
+
+        /// <summary>
+        /// Event raised when the device sample rate changes while it's streaming.
+        /// </summary>
+        event IEagleRadioSource_SampleRateChangedEventArgs OnSampleRateChanged;
+
+        /// <summary>
+        /// Event raised when samples are dropped because the radio can't keep up.
+        /// </summary>
+        event IEagleRadioSource_SamplesDroppedEventArgs OnSamplesDropped;
 
         /// <summary>
         /// Reads a block of samples from the source and returns the amount read.
